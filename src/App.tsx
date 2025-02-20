ace84b131669bda16cdc7676e34b2c20015ec431
satwikWiz <satwik@wizcommerce.com>
2025-02-21 01:14:04 +0530

    Revamp -- Started

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import ImageLinks from './assets';
+import CustomText from './Common/@the-source/CustomText';
+import CursorDispatcher from './Delta/CursorDispatcher';
+import MainAction from './Pages/Display_Hub/__MainAction';
+
+const App = () => {
+  return (
+    <div className="App w-full md:p-6 p-2">
+      {/* <CursorDispatcher /> */}
+      <MainAction imageLink={ImageLinks?.__Elite} captionDisplay='Power Elite Author' titleRender='The all‐in‐one
+WordPress theme to
+build beautiful sites.' subDivisionSection  __scrapButton={'Get Ohio | $59'}/>
+    </div>
+  );
+};
+
+export default App;
